3e961b915da1b8b5c54c7623f1b9dfba9736176c
Arthur <71463136+ta191@users.noreply.github.com>
2022-09-21 09:00:09 -0300

    News mudada de India para Brasil

--- Scrap_trending_news.py
+++ Scrap_trending_news.py
@@ -1,12 +1,13 @@
 from GoogleNews import GoogleNews
 import pandas as pd
 news = GoogleNews(period='1d')
-news.search("India")
+news.search("Brasil")
 result = news.result()
 data = pd.DataFrame.from_dict(result)
 data = data.drop(columns=["img"])
 data.head()
+print(data)
 for i in result:
     print("Title : ", i["title"])
     print("News : ", i["desc"])
-    print("Read Full News : ", i["link"])
+    print("Read/watch full news : ", i["link"])
